8da4d6d526818110524f97da98a9f723c9997462
Sean B. <sdbazoes@gmail.com>
2025-11-20 23:59:39 -0500

    add fixed quadcopter.pyp

--- src/quadcopter.py
+++ src/quadcopter.py
@@ -1,7 +1,30 @@
 from ctypes import *
+import ctypes
+import os
+import time
+
+##### TO RUN ON UNO Q:
+# lib_path = os.path.join(os.path.dirname(__file__), "libquadcopter.so")
+# lib = ctypes.CDLL(lib_path)
+
+##### TO RUN LOCALLY:
 lib = cdll.LoadLibrary('/home/seanb/Documents/quadsquad2026/build/libquadcopter.so')
 
+##### Ensure every data type for every function is set, or it won't work on the Uno Q (but may work locally)
 lib.Quadcopter_setHeight.argtypes = [c_void_p, c_double]
+lib.Quadcopter_setHeight.restype = None
+
+lib.Quadcopter_frontMotorVel.argtypes = [c_void_p]
+lib.Quadcopter_frontMotorVel.restype = c_double
+
+lib.Quadcopter_new.argtypes = []
+lib.Quadcopter_new.restype = c_void_p
+
+lib.Quadcopter_update_simulation.argtypes = [c_void_p]
+lib.Quadcopter_update_simulation.restype = None
+
+lib.Quadcopter_printState.argtypes = [c_void_p]
+lib.Quadcopter_printState.restype = None
 
 class Quadcopter(object):
     def __init__(self):
@@ -31,9 +54,18 @@ class Quadcopter(object):
     def rearMotorVel(self):
         return lib.Quadcopter_rearMotorVel(self.obj)
 
-qc = Quadcopter()
 
-qc.setHeight(1.0)
-while(True):
+def main():
+    print("1")
+    qc = Quadcopter()
+    print(2)
+    # qc.setHeight(1.0)
+    # while(True):
+    print(qc.frontMotorVel())
     qc.update_simulation()
     qc.printState()
+    time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    main()
